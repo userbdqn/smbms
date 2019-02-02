3d0cb66ec28ca7c68bfb44af15e92f3cf2f0d188
Administrator <Administrator@PC-201804142127>
2019-02-02 12:00:12 +0800

    第二次修改

---
+++ src/b/B.java
@@ -0,0 +1,13 @@
+package b;
+
+public class B {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("aabb");
+		System.out.println("vv");
+		
+		}
+	
+
+}
